e76a60f3ccb8ed1f3df7c3a54a16a5f708378444
Roger Huntley <rogerh@hesonline.com>
2023-03-02 18:53:04 -0500

    add some options to commandline

--- README.md
+++ README.md
@@ -30,6 +30,22 @@ then, copy .env.example to .env and fill in the values
 
 -> response to command
 
+#### options:
+
+you can change the max_tokens and temperature
+
+`bun run commandline <command> --max_tokens 100 --temperature 0.5`
+
+alternatively, you can use the following flags:
+
+```
+--short: 10 tokens
+--normal: 100 tokens
+--medium: 500 tokens
+--long: 1000 tokens
+--full: 4000 tokens
+```
+
 #### examples:
 
 `bun run commandline echo hello world`
--- src/scripts/commandline.js
+++ src/scripts/commandline.js
@@ -1,15 +1,66 @@
 import { simplePrompt } from "connect/connect.js";
 
-console.log(process.argv)
-
 // eslint-disable-next-line no-unused-vars
 const [bun, script, ...args] = process.argv
+
+const options = {
+  max_tokens: 100,
+  temperature: 0.7
+}
+
+const availableOptions = [
+  '-m', '--max-tokens',
+  '-t', '--temperature',
+  '--short',
+  '--normal',
+  '--medium',
+  '--long',
+  '--full'
+]
+
+while(args.length > 0 && availableOptions.includes(args[0])) {
+  const option = args.shift()
+  switch(option) {
+    case '-m':
+    case '--max-tokens':
+      options.max_tokens = args.shift()
+      console.log(`setting max tokens to: ${options.max_tokens}`)
+      break
+    case '-t':
+    case '--temperature':
+      options.temperature = args.shift()
+      console.log(`setting temperature to: ${options.temperature}`)
+      break
+    case '--short':
+      options.max_tokens = 10
+      console.log(`setting max tokens to: ${options.max_tokens}`)
+      break
+    case `--normal`:
+      options.max_tokens = 100
+      console.log(`setting max tokens to: ${options.max_tokens}`)
+      break
+    case '--medium':
+      options.max_tokens = 500
+      console.log(`setting max tokens to: ${options.max_tokens}`)
+      break
+    case '--long':
+      options.max_tokens = 1000
+      console.log(`setting max tokens to: ${options.max_tokens}`)
+      break
+    case '--full':
+      options.max_tokens = 4000
+      console.log(`setting max tokens to: ${options.max_tokens}`)
+      break
+    default:
+      break
+  }
+}
 
 const prompt = args.join(" ")
 
 console.log(`prompt: ${prompt}`)
 console.log('sending prompt...')
 
-const response = await simplePrompt(prompt)
+const response = await simplePrompt(prompt, options)
 
 console.log(`response: ${response}`)
