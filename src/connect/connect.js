004a6205dc3b33028b80bb7029776276f9fafd2c
Roger Huntley <rogerh@hesonline.com>
2023-03-03 15:27:44 -0500

    add ra and discord bot character limitation

--- src/connect/connect.js
+++ src/connect/connect.js
@@ -17,15 +17,29 @@ const defaultOptions = {
   model: process.env.USE_MODEL || 'gpt-3.5-turbo',
 }
 
-const basePrompt = async (options) => {
+const basePrompt = async (_options) => {
+  const options = {
+    max_tokens: _options.max_tokens || defaultOptions.max_tokens,
+    temperature: _options.temperature || defaultOptions.temperature,
+    model: _options.model || defaultOptions.model,
+    messages: _options.messages || [],
+  }
+  if(_options.discord){
+    options.messages.push({ role: "system", content: "You must return less than 1800 characters."})
+  }
   const response = await openai.createChatCompletion({
-    ...defaultOptions,
     ...options
   }).then(response => response.data)
 
   saveMessage(response)
   
-  return response.choices[0].message.content
+  let botResponse = response.choices[0].message.content
+
+  if(_options.discord && botResponse.length > 1800){
+    botResponse = botResponse.substring(0, 1800)
+  }
+
+  return botResponse
 }
 
 export { defaultOptions, basePrompt };
---
+++ src/scripts/ra.js
@@ -0,0 +1,5 @@
+import { lawOfOnePrompt } from "../connect/prompts.js";
+
+const ra = await lawOfOnePrompt("tell me about spirit", {discord: true})
+
+console.log(ra)
